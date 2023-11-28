6d04f930189f27aceefcd4239a6e29390ed78b5a
jgrbomb <nrj.serg@gmail.com>
2023-11-28 17:19:13 -0500

    Parent Animal with Children Dog, Lizard, Goldfish

--- ObjectClassEct/Animals.py
+++ ObjectClassEct/Animals.py
@@ -1,6 +1,46 @@
 def main():
-    return
-
+    class Animal:
+        def __init__(self,type, species) -> None:
+            self.type  = type
+            self.species = species
+        def noise(self) -> str:
+            return "..."
+        def __str__(self) -> str:
+            return f"{self.species}({self.type})"
+    class Dog(Animal):
+        def __init__(self, type, species, name) -> None:
+            super().__init__(type, species)
+            self.name = name
+        def noise(self) -> str:
+            return f"{self.name}: BARK!"
+        def __str__(self) -> str:
+            return super().__str__() + "\nName: " + str(self.name)
+    class Lizard(Animal):
+        def __init__(self, type, species, name) -> None:
+            super().__init__(type, species)
+            self.name = name
+        def noise(self) -> str:
+            return f"{self.name}: HISSSSSS!"
+        def __str__(self) -> str:
+            return super().__str__() + "\nName: " + str(self.name)
+    class Goldfish(Animal):
+        def __init__(self, type, species, name) -> None:
+            super().__init__(type, species)
+            self.name = name
+        def noise(self) -> str:
+            return f"{self.name}: BLUB!"
+        def __str__(self) -> str:
+            return super().__str__() + "\nName: " + str(self.name)
+    a = Animal("Amphibian", "Frog")
+    print(a)
+    b = Dog("Mammal","Dog","Spike")
+    print(b)
+    c = Lizard("Reptile", "Lizard", "Lizzy")
+    print(c)
+    d = Goldfish("Fish", "Goldfish", "Shiny")
+    print(d)
+    for i in [a,b,c,d]:
+        print(i.noise())
 
 if __name__ == '__main__':
     main()
